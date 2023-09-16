7ed7f3666385298de24e857a41145481652ec202
Simon Novak <snovak7@gmail.com>
2023-09-16 19:13:54 +0200

    feature: Added OptionsDefaults.cs

---
+++ src/Hosting/Cassandra/OptionsDefaults.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Extensions.Hosting.Cassandra;
+
+/// <summary>
+/// Host Builder Constants.
+/// </summary>
+public static class OptionsDefaults
+{
+    /// <summary>
+    /// Client Key.
+    /// </summary>
+    public const string ClientKey = "Client";
+}
